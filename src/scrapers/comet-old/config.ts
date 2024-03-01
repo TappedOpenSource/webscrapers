f663e28933a7730435b04f44aa986b4e3ea84620
Johannes Naylor <jonaylor89@gmail.com>
2024-02-29 22:32:43 -0500

    add old comet scraper

---
+++ src/scrapers/comet-old/config.ts
@@ -0,0 +1,14 @@
+import { ScraperMetadata } from "../../types";
+
+export const metadata: ScraperMetadata = {
+  id: "UT8F6eUeqzbco8LIBxee2RvKNdc2",
+  name: "comet-old",
+  url: "https://www.cometpingpong.com",
+  sitemap: "https://www.cometpingpong.com/sitemap.xml",
+  location: {
+    lng: -77.06997129999999,
+    geohash: "dqcjw5xqg",
+    placeId: "ChIJ8RQvj7nJt4kRlzTbsADC59s",
+    lat: 38.9559336,
+  },
+};
