41a24806f3e19ed8202fe1b3ea8452d1b2065675
Philip-Hart <71290300+Philip-Hart@users.noreply.github.com>
2024-03-18 20:40:44 -0400

    initialize old town scraper

---
+++ src/scrapers/oldtown/config.ts
@@ -0,0 +1,9 @@
+import { ScraperConfig } from "../../types";
+
+export const config: ScraperConfig = {
+  id: "fgq2S5E4tVN3A4zMtoFW0tBLoLi2",
+  name: "old-town",
+  url: "https://otpsteamboat.com/music-events/",
+  sitemap: "https://otpsteamboat.com/events-sitemap.xml",
+  city: "Steamboat Springs, Colorado",
+};
