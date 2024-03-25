4d4c52e76877fa62b64d45239d96ca90eff9b8aa
Samsung Galaxy <pro@Samsungs-MacBook-Pro.local>
2024-03-25 17:54:01 -0400

    commit

---
+++ src/scrapers/brooklyn-made-productions/config.ts
@@ -0,0 +1,15 @@
+import { ScraperMetadata } from "../../types";
+
+export const metadata: ScraperMetadata = {
+  // id: "UT8F6eUeqzbco8LIBxee2RvKNdc2",
+  name: "brooklynmadepresents",
+  url: "https://brooklynmadepresents.com/",
+  sitemap: "https://brooklynmadepresents.com/sitemap_index.xml",
+  location: {
+    // lng: -77.06997129999999,
+    // geohash: "dqcjw5xqg",
+    // placeId: "ChIJ8RQvj7nJt4kRlzTbsADC59s",
+    // lat: 38.9559336,
+  },
+};
+
---
+++ src/scrapers/brooklyn-made-productions/scraper.ts
@@ -0,0 +1,142 @@
+import type { Browser } from "puppeteer";
+import puppeteer from "puppeteer";
+import Sitemapper from "sitemapper";
+import { metadata } from "./config";
+import { ScrapedEventData } from "../../types";
+import { v4 as uuidv4 } from "uuid";
+import {
+  endScrapeRun,
+  getLatestRun,
+  saveScrapeResult,
+  startScrapeRun,
+} from "../../utils/database";
+import {
+  notifyOnScrapeFailure,
+  notifyOnScrapeSuccess,
+} from "../../utils/notifications";
+import { configDotenv } from "dotenv";
+
+async function getArtists(page: puppeteer.Page): Promise<string[]> {
+  const artistElements = await page.$$eval(
+    ".tribe-events-single-event-title",
+    (elements) => elements.map((el) => el.textContent?.trim() ?? "")
+  );
+  return artistElements;
+}
+
+async function getDate(page: puppeteer.Page): Promise<string[]> {
+  const dateElements = await page.$$eval(
+    ".tribe-event-date-start",
+    (elements) => elements.map((el) => el.textContent?.trim() ?? "")
+  );
+  return dateElements;
+}
+
+async function getTime(page: puppeteer.Page): Promise<string[]> {
+  const timeElements = await page.$$eval(
+    ".tribe-events-schedule h3",
+    (elements) => elements.map((el) => el.textContent?.trim() ?? "")
+  );
+  return timeElements;
+}
+
+async function scrapeEvent(
+  browser: Browser,
+  url: string
+): Promise<ScrapedEventData | null> {
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const eventArtists: string[] = await getArtists(page);
+  const eventDate: string[] = await getDate(page);
+  const eventTime: string[] = await getTime(page);
+
+  // Combine date and time and convert to Date object
+  const combinedDate: string = eventDate.join(" ");
+  const combinedTime: string = eventTime.join(", ");
+  const dateTimeString: string = `${combinedDate} ${combinedTime}`;
+  const dateTime: Date = new Date(dateTimeString);
+
+  const id = uuidv4();
+
+  return {
+    id,
+    url,
+    isMusicEvent: true,
+    title: eventArtists.join(", "),
+    description: "",
+    ticketPrice: null,
+    doorPrice: null,
+    artists: eventArtists,
+    startTime: dateTime,
+    endTime: dateTime,
+    flierUrl: null,
+  };
+}
+
+export async function scrape({ online }: { online: boolean }): Promise<void> {
+  console.log(`[+] scraping [online: ${online}]`);
+  const latestRun = await getLatestRun(metadata);
+  const runId = online ? await startScrapeRun(metadata) : "test-run";
+
+  try {
+    const lateRunStart = latestRun?.startTime ?? null;
+    const lastmod = lateRunStart?.getTime();
+    const sitemap = new Sitemapper({
+      url: metadata.sitemap,
+      lastmod,
+      timeout: 30000,
+    });
+
+    const { sites } = await sitemap.fetch();
+
+    console.log("[+] urls:", sites.length);
+
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+
+    for (const url of sites) {
+      try {
+        const data = await scrapeEvent(browser, url);
+        if (data === null) {
+          console.log("[-] failed to scrape data");
+          continue;
+        }
+
+        if (online) {
+          await saveScrapeResult(metadata, runId, data);
+        }
+      } catch (e) {
+        console.log("[!!!] error:", e);
+        continue;
+      }
+    }
+    await browser.close();
+
+    if (online) {
+      await endScrapeRun(metadata, runId, { error: null });
+      await notifyOnScrapeSuccess({
+        runId,
+        eventCount: sites.length,
+      });
+    }
+  } catch (err: any) {
+    console.log("[-] error:", err);
+    await endScrapeRun(metadata, runId, { error: err.message });
+    if (online) {
+      await notifyOnScrapeFailure({
+        error: err.message,
+      });
+    }
+    throw err;
+  }
+}
+
+if (require.main === module) {
+  configDotenv({
+    path: ".env",
+  });
+
+  scrape({ online: false });
+}
